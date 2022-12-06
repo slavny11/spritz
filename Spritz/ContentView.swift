7a5b8a3be7e621f7ba08e93d3610318ba0cd8051
slavny11 <slavny45@gmail.com>
2022-12-06 14:17:14 +0100

    Set up background and pictures on the main screen. Added font Created Storyboard

--- Spritz/ContentView.swift
+++ Spritz/ContentView.swift
@@ -6,21 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        storyboardView().edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct storyboardView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let controller = storyboard.instantiateViewController(identifier: "Home")
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
     }
 }
---
+++ Spritz/StoryBoard/SpritzViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SpritzViewController.swift
+//  Spritz
+//
+//  Created by Viacheslav on 06/12/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class SpritzViewController: UIViewController {
+    
+    
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .portrait }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func printBTN(_ sender: Any) {
+        print("Wow, this is storyboard")
+    }
+}
