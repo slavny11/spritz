014d612fafa03ae87e07b203d80e170e7ac8cc85
slavny11 <slavny45@gmail.com>
2022-12-06 10:34:12 +0100

    Initial Commit

---
+++ Spritz/SpritzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpritzApp.swift
+//  Spritz
+//
+//  Created by Viacheslav on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SpritzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
